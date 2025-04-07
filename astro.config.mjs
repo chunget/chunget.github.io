7769c044bad79c4be93736c3bc89b0800abaded9
Jill <jill.chung@gigabyte.com>
2025-04-07 15:47:55 +0800

    變更執行頁

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from '@astrojs/tailwind';
 
 /*
 export default defineConfig({
-  
+    //integrations: [tailwind()],
 });
 */
 export default defineConfig({
-  integrations: [tailwind()],
+
   site:'https://chunget.github.io',
   base:'/astro-project'
 });
