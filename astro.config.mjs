5afcb7e5a1841aa04ea9d209e36d75b2b4863acf
Jill Chung <chunget@gmail.com>
2025-04-09 18:04:54 +0800

    更新 logo 和部署配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-/*
-export default defineConfig({
-    //integrations: [tailwind()],
-});
-*/
-export default defineConfig({
 
-  site:'https://chunget.github.io',
-  base:'/astro-project'
+export default defineConfig({
+    integrations: [tailwind()],
 });
+
+/*export default defineConfig({
+  site:'https://chunget.github.io',
+  base:'/astro-project',
+  integrations: [tailwind()]
+});*/
